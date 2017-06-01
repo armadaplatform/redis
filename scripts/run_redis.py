32bb7e6ae1c36be7e2c0dab7977a860963042c1b
Filip Wójciak <fwojciak@ganymede.eu>
2017-06-01 10:21:38 +0200

    Simplify generating redis config.

--- scripts/run_redis.py
+++ scripts/run_redis.py
@@ -14,29 +14,27 @@ DEFAULT_CONFIG = {
 
 
 def create_redis_conf_section_from_dict(config):
-    lines = ['# Config values added by redis armada service:']
+    lines = ['# Config values added by redis armada service:\n']
     for k, v in config.items():
-        if isinstance(v, list):
-            v = ' '.join(map(str, v))
-        line = f'{k} {v}'
-        lines.append(line + '\n')
+        lines.append(f'{k} {v}\n')
     return ''.join(lines)
 
 
 def main():
-    os.environ['CONFIG_PATH'] += ':/opt/redis/'
     microservice_name = os.environ.get('MICROSERVICE_NAME')
     if 'slave' in microservice_name:
         mode = 'slave'
-        DEFAULT_CONFIG['slaveof'] = ['127.0.0.1', '10003']
+        DEFAULT_CONFIG['slaveof'] = '127.0.0.1 10003'
     else:
         mode = 'master'
+
     config = DEFAULT_CONFIG
     config.update(hermes.get_config(f'redis.{mode}.conf.json'))
     shutil.copyfile('/tmp/redis-stable/redis.conf', REDIS_CONFIG_PATH)
     with open(REDIS_CONFIG_PATH, 'a') as f:
-        new_config_section = create_redis_conf_section_from_dict(config)
-        f.write(new_config_section)
+        armada_config_section = create_redis_conf_section_from_dict(config)
+        f.write(armada_config_section)
+
     os.execl('/usr/local/bin/redis-server', 'redis-server', REDIS_CONFIG_PATH)
 
 
